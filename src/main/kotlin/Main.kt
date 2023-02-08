e7d76a32686d44990553efdf121f03e548545d00
student <eunicemusenyia@gmail.com>
2023-02-08 08:20:30 +0300

    added new functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+var Greetings ="Hello Eunice"
+    println("Hello Eunice")
+    var remainder = divideTwoNumbers(319,3)
+    println(remainder)
+
+   var sum = addTwoNumbers (133,221,356, 879)
+
+    println(sum)
+
+    println("I am a beautiful work in progress")
+
+
+
+
+}
+fun divideTwoNumbers(num1:Int, num2:Int):Int{
+    var remainder = num1 % num2
+    return remainder
+
+}
+fun addTwoNumbers(num1: Int, num2: Int, num3: Int, num4: Int):Int{
+    var sum = num1 + num2 + num3 + num4
+    return sum
+}
+fun aboutMyself(){
+        var interestingFact = "I am a beautiful work in progress"
+}
